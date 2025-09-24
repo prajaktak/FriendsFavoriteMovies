dfd5c3208083ca738fd84003ae7a2e761ae502fc
Prajakta Kulkarni <your.email@example.com>
2025-09-24 23:03:59 +0200

    created a app with tabView to display friend list and movie list. also added navigation to show detail view of fried and movie using navigation link
    rebased navigation brach

---
+++ FriendsFavoriteMovies/FriendList.swift
@@ -0,0 +1,39 @@
+//
+//  FriendList.swift
+//  FriendsFavoriteMovies
+//
+//  Created by Prajakta Kulkarni on 25/09/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+struct FriendList: View {
+    @Query(sort: \Friend.name) private var friends: [Friend]
+    @Environment(\.modelContext) private var context
+    
+    var body: some View {
+        NavigationSplitView {
+            List {
+                ForEach(friends) { friend in
+                    NavigationLink(friend.name) {
+                        Text("Detail view for \(friend.name)")
+                            .navigationTitle("Friend")
+                            .navigationBarTitleDisplayMode(.inline)
+                    }
+                }
+            }
+            .navigationTitle("Friends")
+        } detail: {
+            Text("Select a friend")
+                .navigationTitle("Friend")
+                .navigationBarTitleDisplayMode(.inline)
+        }
+        
+    }
+}
+
+#Preview {
+    FriendList()
+        .modelContainer(SampleData.shared.modelContainer)
+}
